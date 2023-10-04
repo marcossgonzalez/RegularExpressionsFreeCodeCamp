0f0318e6a9287c22f56468961841b4a373632288
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-03 19:51:56 -0700

    Update lesson
    update lesson

--- testmethod.js
+++ testmethod.js
@@ -4,9 +4,9 @@ JavaScript tem várias maneiras de usar expressões regulares. Uma maneira de te
 O método .test() pega o regex, aplica-o a uma string (que é colocada entre parênteses) e 
 retorna verdadeiro ou falso se o seu padrão encontrar algo ou não.*/
 //------------------------------
-let myString = "Hello, World!";
+let myString1 = "Hello, World!";
 let myRegex = /Hello/;
-let result = myRegex.test(myString);
+let result1 = myRegex.test(myString);
 //------------------------------
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; // Change this line
@@ -21,8 +21,31 @@ let result3 = petRegex.test(petString);
 //------------------------------
 let myString = "freeCodeCamp";
 let fccRegex = /freeCodeCamp|FreeCOdeCamp|FrEeCoDeCamp/i; // Change this line
-let result = fccRegex.test(myString);
+let result4 = fccRegex.test(myString);
 /*
 Você pode usá-lo anexando-o ao regex. Um exemplo de uso deste sinalizador 
 é /ignorecase/i. Este regex pode corresponder às strings ignorecase, igNoreCase e IgnoreCase*/
+//------------------------------Extract Matches------------------------------
+/*Se um padrão existe ou não dentro de uma string você também 
+pode extrair as correspondências reais encontradas com o método .match().
+Ex:
+"Hello, World!".match(/Hello/);
+
+ou 
+
+let ourStr = "Regular expressions";
+let ourRegex = /expressions/;
+ourStr.match(ourRegex);
+
+*/
+//------------------------------
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/;
+let result5 = extractStr.match(codingRegex);
+//------------------------------Encontre mais do que a primeira entrada -----------------------
+/* Para pesquisar ou extrair um padrão mais de uma vez, 
+você pode usar o sinalizador de pesquisa global: g.
+Ex: /Hello/g; */
+//------------------------------
+
 //------------------------------
