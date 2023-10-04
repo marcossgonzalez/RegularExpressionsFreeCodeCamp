3e2334069c8497eb9eeac4cf177a228a71a28f12
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-04 11:13:57 -0700

    Lesson update
    lesson update

--- stringpatterns.js
+++ stringpatterns.js
@@ -45,4 +45,45 @@ let result6 = movieName.match(noNumRegex).length;
 //--------------------------------------------------
 let username = "JackOfAllTrades";
 let userCheck = /^([a-zA-Z]{2,})+(\d*$)|^[a-zA-Z]([0-9]{2,})$/i; // Change this line
-let result = userCheck.test(username);
+let result7 = userCheck.test(username);
+/*Você pode pesquisar espaços em branco usando \s, 
+que é um s minúsculo. Esse padrão não corresponde apenas a 
+espaços em branco, mas também a caracteres de retorno de carro, 
+tabulação, feed de formulário e nova linha. 
+Você pode pensar nisso como semelhante à classe de personagem [\r\t\f\n\v].*/
+//--------------------------------------------------
+let sample2 = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result8 = sample2.match(countWhiteSpace).length;
+//--------------------------------------------------
+/*Procure por espaços que não sejam em branco usando '\S', que é um s maiúsculo. 
+Este padrão não corresponderá a espaços em branco, retorno de carro, tabulação, 
+feed de formulário e caracteres de nova linha. 
+Você pode pensar que é semelhante à classe de personagem [^ \r\t\f\n\v].*/
+//--------------------------------------------------
+let sample3 = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g; // Change this line
+let result9 = sample3.match(countNonWhiteSpace).length;
+//--------------------------------------------------
+/*Você pode especificar o número inferior e superior de padrões com especificadores 
+de quantidade. Os especificadores de quantidade são usados com chaves ({ e }). 
+Você coloca dois números entre as chaves - para o número inferior e superior de padrões.*/
+//--------------------------------------------------
+let ohStr = "Ohhh no";
+let ohRegex = /Oh{3,6} no/g; // Change this line
+let result10 = ohRegex.test(ohStr);
+//--------------------------------------------------
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/gi; // Change this line
+let result11 = haRegex.test(haStr);
+//--------------------------------------------------
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/gi; // Change this line
+let result12 = timRegex.test(timStr);
+//--------------------------------------------------
+let favWord = "favorite";
+let favRegex = /favou?rite/gi; // Change this line
+let result = favRegex.test(favWord);
+//--------------------------------------------------
+
+//--------------------------------------------------
